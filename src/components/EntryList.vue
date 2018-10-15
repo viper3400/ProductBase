bf30b7fc3b6e7aed9db4f1f79ba76679cb930b54
viper3400 <viper_3400@gmx.de>
2018-10-15 19:21:38 +0200

    show list entries

---
+++ src/components/EntryList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <background-image />
+    <div class="overlay">
+      <app-bar/>
+      <div class="page-wrap sizing">
+        <mu-list v-if="products">
+          <mu-list-item v-for="(item, idx) in products" :key="idx">
+            <mu-list-item-title>{{item.name}} ({{item.rating}})</mu-list-item-title>
+          </mu-list-item>
+        </mu-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+import AppBar from '@/Components/AppBar'
+import BackgroundImage from '@/Components/BackgroundImage'
+
+export default {
+  name: 'EntryList',
+  data () {
+    return {
+      products: []
+    }
+  },
+  components: {
+    BackgroundImage,
+    AppBar
+  },
+  methods: {
+    readData: function () {
+      var ref = firebase.database().ref('products')
+      ref.once('value').then((snapshot) => {
+        this.products = snapshot.val()
+      })
+    }
+  },
+  mounted () {
+    this.readData()
+  }
+}
+</script>
+
+<style scoped>
+.sizing {
+  width: 60%;
+  margin: 0 auto;
+  margin-top: 5%;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import CreateEntry from '@/components/CreateEntry'
+import EntryList from '@/components/EntryList'
 import Login from '@/components/Login'
 
 Vue.use(Router)
@@ -16,6 +17,11 @@ export default new Router({
       path: '/createentry',
       name: 'CreateEntry',
       component: CreateEntry
+    },
+    {
+      path: '/entrylist',
+      name: 'EntryList',
+      component: EntryList
     }
   ]
 })

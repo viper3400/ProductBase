67b905feebcb391fb9a766263f9980012f87d932
viper3400 <viper_3400@gmx.de>
2018-10-16 18:42:25 +0200

    implemented signout

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -7,23 +7,32 @@
   <mu-menu slot="right">
     <mu-button flat>MENU</mu-button>
     <mu-list slot="content">
-      <mu-list-item button>
+      <mu-list-item button @click="signOut">
         <mu-list-item-content>
-          <mu-list-item-title>Menu Item 1</mu-list-item-title>
+          <mu-list-item-title>SignOut</mu-list-item-title>
         </mu-list-item-content>
       </mu-list-item>
-      <mu-list-item button>
+      <!--<mu-list-item button>
         <mu-list-item-content>
           <mu-list-item-title>Menu Item 2</mu-list-item-title>
         </mu-list-item-content>
-      </mu-list-item>
+      </mu-list-item>-->
     </mu-list>
   </mu-menu>
 </mu-appbar>
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
-  name: 'AppBar'
+  name: 'AppBar',
+  methods: {
+    signOut: function () {
+      firebase.auth().signOut().then(() => {
+        this.$router.replace('/')
+      })
+    }
+  }
 }
 </script>

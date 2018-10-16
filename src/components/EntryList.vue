2056a01fc90e5c7cae677ed1c2fdae3020f0c735
viper3400 <viper_3400@gmx.de>
2018-10-16 19:52:10 +0200

    entry list with cards

--- src/components/EntryList.vue
+++ src/components/EntryList.vue
@@ -4,11 +4,31 @@
     <div class="overlay">
       <app-bar/>
       <div class="page-wrap sizing">
-        <mu-list v-if="products">
+        <!--<mu-list v-if="products">
           <mu-list-item v-for="(item, idx) in products" :key="idx">
             <mu-list-item-title  @click="editRedirect(idx)">{{item.name}} ({{item.rating}})</mu-list-item-title>
           </mu-list-item>
-        </mu-list>
+        </mu-list>-->
+      <mu-container v-if="products">
+        <mu-row gutter>
+          <mu-col span="4" v-for="(item, idx) in products" :key="idx">
+            <mu-card style="width: 100% height: 50px; margin: 0 auto;">
+             <!--<mu-card-header :title="item.name" sub-title="sub title">
+              </mu-card-header>-->
+              <mu-card-media>
+                <img width="120px" src="https://d17gnwn89zo776.cloudfront.net/media/catalog/product/cache/1/image/1800x/040ec09b1e35df139433887a97daa66f/g/i/gin-mare-mediterranean-gin-70cltest.jpg">
+              </mu-card-media>
+              <mu-card-title :title="item.name" :sub-title="item.rating"></mu-card-title>
+              <mu-card-text>
+              </mu-card-text>
+              <mu-card-actions>
+                <mu-button flat @click="editRedirect(idx)">Edit</mu-button>
+                <mu-button flat>Remove</mu-button>
+              </mu-card-actions>
+            </mu-card>
+          </mu-col>
+      </mu-row>
+      </mu-container>
       </div>
     </div>
   </div>
@@ -53,4 +73,8 @@ export default {
   margin: 0 auto;
   margin-top: 5%;
 }
+
+.col {
+    margin-bottom: 18px;
+    }
 </style>

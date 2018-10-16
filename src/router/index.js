a98a88eb1d019095927754ed1f1022aa9f459a68
viper3400 <viper_3400@gmx.de>
2018-10-16 18:47:37 +0200

    Full implementation of auth

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,11 @@ import Router from 'vue-router'
 import CreateEntry from '@/components/CreateEntry'
 import EntryList from '@/components/EntryList'
 import Login from '@/components/Login'
+import firebase from 'firebase'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
     {
       path: '/',
@@ -16,12 +17,29 @@ export default new Router({
     {
       path: '/createentry',
       name: 'CreateEntry',
-      component: CreateEntry
+      component: CreateEntry,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/entrylist',
       name: 'EntryList',
-      component: EntryList
+      component: EntryList,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  let currentUser = firebase.auth().currentUser
+  let requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+
+  if (requiresAuth && !currentUser) next('/')
+  else if (!requiresAuth && currentUser) next('entryList')
+  else next()
+})
+
+export default router

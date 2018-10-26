f71b997ab92388d0abaa0d08bba8139ffebbfa84
viper3400 <viper_3400@gmx.de>
2018-10-26 18:30:15 +0200

    Removed unused html code

--- src/components/EntryList.vue
+++ src/components/EntryList.vue
@@ -33,36 +33,6 @@
           </mu-col>
         </mu-row>
       </mu-container>
-      <!--mu-container v-if="products">
-        <mu-row>
-          <mu-col v-for="(item, idx) in products" :key="idx">
-            <div class="wrapper">
-              <div class="product-img">
-                <img v-if="item.img" :src="item.img" height="420" width="auto">
-              </div>
-              <div class="product-info">
-                <div class="product-text">
-                  <vue-stars :name="'star' + idx" class="product-text-stars" :value="item.rating" :readonly="true" shadowColor="black"/>
-                  <h1>{{item.brand}}</h1>
-                  <h2 v-if="item.name">{{item.name}}</h2>
-                  <p>
-                  {{$ml.get('fillingQuantity')}}: {{item.fillingQuantity}}<br>
-                  {{$ml.get('alcoholStrength')}}: {{item.alcoholStrength}}<br>
-                  {{$ml.get('price')}}: {{item.price}}<br>
-                  {{$ml.get('description')}}: {{item.description}}</p>
-                </div>
-                <mu-container>
-                  <mu-flex justify-content="end">
-                <mu-button @click="editRedirect(item['.key'])" small fab color="indigo400">
-                  <mu-icon value="edit"></mu-icon>
-                </mu-button>
-                  </mu-flex>
-                </mu-container>
-              </div>
-           </div>
-          </mu-col>
-        </mu-row>
-      </mu-container-->
       </div>
     </div>
   </div>
